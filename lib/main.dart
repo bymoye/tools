b039932234d48f654490e4620ae04049f8eb1219
bymoye <s3moye@gmail.com>
2024-12-22 20:15:47 +0800

    path_url

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:tools/global_variable.dart';
 import 'package:tools/router.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
   runApp(const MainApp());
 }
 
